129d4001ba6799ac33d0ecd4d526b45bbd5d4e09
sainthkh <sainthkh@naver.com>
2017-01-23 12:03:52 +0900

    Added signup and welcome mail feature.

--- web/index.js
+++ web/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const http = require('http');
+const https = require('https')
+const qs = require('querystring')
 
 let config = JSON.parse(fs.readFileSync(`./config.json`).toString())
 let layout = fs.readFileSync(`./themes/${config.theme}/layout.html`).toString()
@@ -14,6 +16,8 @@ http.createServer((req, res) => {
 			return serveFav(res)
 		case '/sync':
 			return sync(res)
+		case '/subscribe':
+			return subscribe(req, res)
 		default:
 			return servePosts(req.url, res)
 	}
@@ -65,6 +69,105 @@ function sync(res) {
 	res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
 	res.write('Sync done.')
 	return res.end()
+}
+
+function subscribe(req, res) {
+	if (req.method == 'POST') {
+		var body = '';
+
+		req.on('data', function (data) {
+			body += data;
+
+			// Too much POST data, kill the connection!
+			// 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+			if (body.length > 1e6)
+				request.connection.destroy();
+		});
+
+		req.on('end', function () {
+			let { email, firstName } = qs.parse(body);
+			addSubscriber(email, firstName)
+			sendWelcomeMail(email, firstName)
+		});
+	}
+	res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
+	res.write(`<h1>Thank you for subscription. I'll talk to you soon. </h1>`)
+	return res.end()
+}
+
+function addSubscriber(email, firstName) {
+	let payload = qs.stringify({
+		address: email,
+		name: firstName,
+	})
+
+	let req = https.request({
+		host: "api.mailgun.net",
+		port: 443,
+		path: `/v3/lists/all@${config.domain}/members`,
+		auth: `api:${config.key}`,
+		method: "POST",
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': Buffer.byteLength(payload),
+		}
+	}, (res) => {
+		console.log('STATUS: ' + res.statusCode);
+		console.log('HEADERS: ' + JSON.stringify(res.headers));
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+			console.log('BODY: ' + chunk);
+		});
+	})
+
+	req.write(payload)
+	req.end()
+}
+
+function sendWelcomeMail(email, firstName) {
+	let payload = qs.stringify({
+		from: config.from,
+		to: email,
+		subject: "Thank you for subscription",
+		html: [
+			`<div style="color:#222222;font-family:'Helvetica','Arial',sans-serif;font-size:14px;line-height:1.4;padding:25px;width:550px">`,
+			`<p style="margin-bottom:16px">Hello, ${firstName}.</p>`,
+			`<p style="margin-bottom:16px">As we all know, it's not a good idea to get email address by just telling the readers "enter your email address if you want updates".</p>`,
+			`<p style="margin-bottom:16px">However, you signed up. Thank you for that. It seems that you really care my contents.</p>`,
+			`<p style="margin-bottom:16px">I will try my best to share my journey with you.</p>`,
+			`<p style="margin-bottom:16px">Sincerely,<br>`,
+			`Kukhyeon</p>`,
+			`</div>`,
+			`<div style="border-top-color:#ddd;border-top-style:solid;border-top-width:1px;color:#888;font-family:'Helvetica','Arial',sans-serif;font-size:12px;line-height:1.4;padding:25px;width:550px">`,
+			`To make sure you keep getting these emails, please add admin@${config.domain} to your address book or whitelist us.<br>`,
+			`Want out of the this email list? <a href="%mailing_list_unsubscribe_url%">Unsubscribe this list</a><br>`,		
+			`Want to unsubscribe every email? <a href="%unsubscribe_url%">Click here</a><br>`,
+			`Postal Address: ${config.postalAddress}`,
+			`</div>`
+		].join(``),
+	})
+
+	let req = https.request({
+		host: "api.mailgun.net",
+		port: 443,
+		path: `/v3/${config.domain}/messages`,
+		auth: `api:${config.key}`,
+		method: "POST",
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': Buffer.byteLength(payload),
+		}
+	}, (res) => {
+		console.log('STATUS: ' + res.statusCode);
+		console.log('HEADERS: ' + JSON.stringify(res.headers));
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+			console.log('BODY: ' + chunk);
+		});
+	})
+
+	req.write(payload)
+	req.end()
 }
 
 function serveFav(res) {
